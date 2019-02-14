169906dbd17811989944a06d4154e53de564ac3a
sookmyung <@>
2019-02-14 15:31:08 +0900

    Initial commit

---
+++ app/src/main/java/com/example/sookmyung/database2/Beerclicked.java
@@ -0,0 +1,13 @@
+package com.example.sookmyung.database2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Beerclicked extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beerclicked);
+    }
+}
